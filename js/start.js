8d8053d918d0292f6406cfa4c9455f0a5c83efed
Anen <bastien.lebayle@gmail.com>
2020-02-29 21:31:56 -0500

    Update 2020

--- js/start.js
+++ js/start.js
@@ -30,14 +30,28 @@ document.addEventListener('DOMContentLoaded', function () {
   	var sels = document.getElementsByTagName('a');
   	for(i = 0; i < sels.length; i++) {
 		sels[i].addEventListener('click', 
-										 function(){ clickHandler(this.innerText); } 
-										 );
+							     function(){ clickHandler(this.innerText); } );
   	}
 });    
  
 window.onload = function() {  
 	console.log("Calling OnLoad()...");
 	//localStorage.clear();
-	document.getElementById("history_ta").innerHTML = localStorage.history;
+	if (document.getElementById("history_ta") != null) {
+		document.getElementById("history_ta").innerHTML = localStorage.history;
+	}
+	
+
+	// Load weather widget
+	!function(d, s, id){
+		var js,fjs = d.getElementsByTagName(s)[0];
+		if(!d.getElementById(id)){
+			js = d.createElement(s);
+			js.id = id;
+			js.src = 'https://weatherwidget.io/js/widget.min.js';
+			fjs.parentNode.insertBefore(js,fjs);
+		}
+	}(document,'script','weatherwidget-io-js');
+	
 };
 
